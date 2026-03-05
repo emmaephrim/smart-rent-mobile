64639393f00a916e5c894f0621bebe1672716cee
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-05 17:12:26 +0000

    feat: home screen contents replaced, design in progress

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:smart_rent_mobile/core/theme/app_gradients.dart';
 import 'package:smart_rent_mobile/core/theme/app_colors.dart';
 import 'package:smart_rent_mobile/core/theme/app_theme.dart';
+import 'package:smart_rent_mobile/features/home/presentation/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,56 +18,7 @@ class MyApp extends StatelessWidget {
       title: 'Smart Rent',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      home: const MyHomePage(title: 'Smart Rent'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.primary,
-        title: Text(widget.title, style: TextStyle(color: AppColors.surface)),
-      ),
-      body: Container(
-        decoration: BoxDecoration(gradient: AppGradients.mainBackground),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: .center,
-            children: [
-              const Text('You have pushed the button this many times:'),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const HomeScreen(isLoggedIn: false),
     );
   }
 }
