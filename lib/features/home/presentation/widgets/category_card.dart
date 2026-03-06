5a75acfe2820252373947a456fc6507a16ddafa7
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-06 09:17:37 +0000

    feat: category card updated for new design

--- lib/features/home/presentation/widgets/category_card.dart
+++ lib/features/home/presentation/widgets/category_card.dart
@@ -3,50 +3,40 @@ import 'package:smart_rent_mobile/core/theme/app_colors.dart';
 import 'package:smart_rent_mobile/core/theme/app_text_styles.dart';
 
 class CategoryCard extends StatelessWidget {
-  final String title;
-  final String imagePath;
+  final String image;
+  final String label;
   final VoidCallback onTap;
 
   const CategoryCard({
     super.key,
-    required this.title,
-    required this.imagePath,
+    required this.image,
+    required this.label,
     required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          color: AppColors.surface,
-          borderRadius: BorderRadius.circular(18),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withValues(
-                alpha: 0.05,
-              ), // TODO: check this line again
-              blurRadius: 10,
-              offset: const Offset(0, 4),
-            ),
-          ],
-        ),
-        padding: EdgeInsets.all(12),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(child: Image.asset(imagePath, fit: BoxFit.contain)),
-            const SizedBox(height: 10),
-            Text(
-              title,
-              style: AppTextStyles.bodyLarge.copyWith(
-                fontWeight: FontWeight.w600,
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: onTap,
+          child: Container(
+            height: 99,
+            width: 99,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                image: AssetImage(image),
+                fit: BoxFit.cover,
               ),
             ),
-          ],
+          ),
         ),
-      ),
+
+        const SizedBox(height: 6),
+
+        Text(label, style: const TextStyle(fontWeight: FontWeight.w500)),
+      ],
     );
   }
 }
