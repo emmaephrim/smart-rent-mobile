c333e5eed349b2f51b40e359f630bfaa842116f9
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-06 10:01:13 +0000

    fix: homescreen background gradient fixed

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_rent_mobile/core/theme/app_gradients.dart';
 import 'package:smart_rent_mobile/core/theme/app_text_styles.dart';
 import 'package:smart_rent_mobile/features/home/presentation/widgets/category_grid.dart';
 import 'package:smart_rent_mobile/features/home/presentation/widgets/home_header.dart';
@@ -11,37 +12,41 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            HomeHeader(
-              isLoggedIn: isLoggedIn,
-              userName: "Emmanuel",
-              role: "RENTER",
-              location: "Cape Coast, Ghana",
-            ),
+      backgroundColor: Colors.transparent,
+      body: Container(
+        decoration: BoxDecoration(gradient: AppGradients.mainBackground),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              HomeHeader(
+                isLoggedIn: isLoggedIn,
+                userName: "Emmanuel",
+                role: "RENTER",
+                location: "Cape Coast, Ghana",
+              ),
 
-            const SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "What can you rent?",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "What can you rent?",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            ),
 
-            // const SizedBox(height: 5),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: CategoryGrid(),
-            ),
+              // const SizedBox(height: 5),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: CategoryGrid(),
+              ),
 
-            const SizedBox(height: 40),
-          ],
+              const SizedBox(height: 40),
+            ],
+          ),
         ),
       ),
     );
