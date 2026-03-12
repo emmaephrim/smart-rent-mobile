88c9196c435260d9666a48aadc7b99f705c26e51
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-12 15:26:41 +0000

    feat: flutter provider state management init

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,42 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:smart_rent_mobile/core/theme/app_gradients.dart';
+import 'package:smart_rent_mobile/features/auth/providers/auth_provider.dart';
 import 'package:smart_rent_mobile/features/home/presentation/widgets/category_grid.dart';
 import 'package:smart_rent_mobile/features/home/presentation/widgets/home_header.dart';
 
-class HomeScreen extends StatefulWidget {
-  final bool isLoggedIn;
-
-  const HomeScreen({super.key, required this.isLoggedIn});
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authState = ref.watch(authProvider);
 
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: Container(
         decoration: BoxDecoration(gradient: AppGradients.mainBackground),
         child: Column(
-          // Use a Column to stack the fixed and scrollable parts
           children: [
-            // 1. FIXED HEADER (Stays at the top)
             HomeHeader(
-              isLoggedIn: widget.isLoggedIn,
-              userName: "Emmanuel",
-              role: "RENTER",
+              isLoggedIn: authState.isLoggedIn,
+              userName: authState.userName ?? "Guest",
+              role: authState.role ?? "GUEST",
               location: "Cape Coast, Ghana",
             ),
 
-            // 2. SCROLLABLE AREA
             Expanded(
-              // Expanded ensures the scroll view takes up the remaining space
               child: SingleChildScrollView(
                 child: Column(
                   children: [
                     const SizedBox(height: 15),
+
                     const Padding(
                       padding: EdgeInsets.symmetric(horizontal: 20),
                       child: Align(
@@ -50,10 +44,12 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       ),
                     ),
+
                     const Padding(
                       padding: EdgeInsets.symmetric(horizontal: 20),
                       child: CategoryGrid(),
                     ),
+
                     const SizedBox(height: 40),
                   ],
                 ),
