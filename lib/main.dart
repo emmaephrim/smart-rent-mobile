ef7b20c1b991f925b8c17c632e589e70572d87d6
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-19 14:35:23 +0000

    feat: go router confifg

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:smart_rent_mobile/core/theme/app_theme.dart';
 import 'package:smart_rent_mobile/features/navigation/presentation/screens/main_navigation_screen.dart';
 
@@ -7,17 +8,35 @@ void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+final goRouterProvider = Provider((ref) {
+  return GoRouter(
+    initialLocation: '/login',
+    routes: [
+      // GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
+      // GoRoute(
+      //   path: '/register',
+      //   builder: (context, state) => const RegisterScreen(),
+      // ),
+      GoRoute(
+        path: '/home',
+        builder: (context, state) => const MainNavigationScreen(),
+      ),
+    ],
+  );
+});
+
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final goRouter = ref.watch(goRouterProvider);
+
+    return MaterialApp.router(
       title: 'Smart Rent',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      home: const MainNavigationScreen(),
+      routerConfig: goRouter,
     );
   }
 }
