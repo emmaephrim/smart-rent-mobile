d305d7a39ac48f28f7bb8a298f3a86e8eb751327
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-19 21:26:35 +0000

    feat: register screen init

---
+++ lib/features/auth/presentation/screens/register_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:smart_rent_mobile/core/theme/app_gradients.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        decoration: const BoxDecoration(gradient: AppGradients.mainBackground),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () => context.pop(),
+                        icon: const Icon(Icons.arrow_back),
+                      ),
+                    ],
+                  ),
+
+                  const Text(
+                    "Sign up",
+                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                  ),
+
+                  const SizedBox(height: 8),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Already have an account? "),
+                      GestureDetector(
+                        onTap: () => context.go('/login'),
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  const TextField(
+                    decoration: InputDecoration(labelText: "First Name"),
+                  ),
+                  const SizedBox(height: 10),
+
+                  const TextField(
+                    decoration: InputDecoration(labelText: "Last Name"),
+                  ),
+                  const SizedBox(height: 10),
+
+                  const TextField(
+                    decoration: InputDecoration(labelText: "Email"),
+                  ),
+                  const SizedBox(height: 10),
+
+                  const TextField(
+                    decoration: InputDecoration(labelText: "Phone Number"),
+                  ),
+                  const SizedBox(height: 10),
+
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: "Password"),
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Register"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
