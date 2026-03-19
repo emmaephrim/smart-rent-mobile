01b9a87116117000c82905b5897a0334b7bffc15
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-19 21:26:23 +0000

    feat: login screen init

---
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:smart_rent_mobile/core/theme/app_gradients.dart';
+import 'package:smart_rent_mobile/features/auth/providers/auth_provider.dart';
+
+class LoginScreen extends ConsumerWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        decoration: const BoxDecoration(gradient: AppGradients.mainBackground),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  const Text(
+                    "Login",
+                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                  ),
+
+                  const SizedBox(height: 8),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Don’t have an account? "),
+                      GestureDetector(
+                        onTap: () => context.go('/register'),
+                        child: const Text(
+                          "Sign Up",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  const TextField(
+                    decoration: InputDecoration(labelText: "Email"),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: "Password"),
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        ref
+                            .read(authProvider.notifier)
+                            .login("Emmanuel", "RENTER");
+                      },
+                      child: const Text("Log In"),
+                    ),
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  const Text("Or"),
+
+                  const SizedBox(height: 20),
+
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Continue with Google"),
+                  ),
+
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Continue with Facebook"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
