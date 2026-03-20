f9cf0ede10811430249d8f0b41018d08ba9c3ef6
Emmanuel Ephrim <emmaephrim@gmail.com>
2026-03-20 21:10:50 +0000

    fix: app brandmark

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -15,80 +15,112 @@ class LoginScreen extends ConsumerWidget {
         decoration: const BoxDecoration(gradient: AppGradients.mainBackground),
         child: Center(
           child: SingleChildScrollView(
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Column(
-                children: [
-                  const Text(
-                    "Login",
-                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset("assets/images/logo.png", height: 40),
+                    const SizedBox(width: 10),
+                    const Text(
+                      "Smart Rent",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 20),
+
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 20),
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
                   ),
-
-                  const SizedBox(height: 8),
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  child: Column(
                     children: [
-                      const Text("Don’t have an account? "),
-                      GestureDetector(
-                        onTap: () => context.go('/register'),
-                        child: const Text(
-                          "Sign Up",
-                          style: TextStyle(color: Colors.blue),
+                      const Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                      const SizedBox(height: 8),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Don’t have an account? "),
+                          GestureDetector(
+                            onTap: () => context.go('/register'),
+                            child: const Text(
+                              "Sign Up",
+                              style: TextStyle(color: Colors.blue),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      const SizedBox(height: 20),
+
+                      const TextField(
+                        decoration: InputDecoration(labelText: "Email"),
+                      ),
+
+                      const SizedBox(height: 10),
+
+                      const TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(labelText: "Password"),
+                      ),
+
+                      const SizedBox(height: 20),
+
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            ref
+                                .read(authProvider.notifier)
+                                .login("Emmanuel", "RENTER");
+                          },
+                          child: const Text("Log In"),
+                        ),
+                      ),
+
+                      const SizedBox(height: 20),
+
+                      const Text("Or"),
+
+                      const SizedBox(height: 20),
+
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text("Continue with Google"),
+                        ),
+                      ),
+
+                      const SizedBox(height: 15),
+
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text("Continue with Facebook"),
                         ),
                       ),
                     ],
                   ),
-
-                  const SizedBox(height: 20),
-
-                  const TextField(
-                    decoration: InputDecoration(labelText: "Email"),
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(labelText: "Password"),
-                  ),
-
-                  const SizedBox(height: 20),
-
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        ref
-                            .read(authProvider.notifier)
-                            .login("Emmanuel", "RENTER");
-                      },
-                      child: const Text("Log In"),
-                    ),
-                  ),
-
-                  const SizedBox(height: 20),
-
-                  const Text("Or"),
-
-                  const SizedBox(height: 20),
-
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text("Continue with Google"),
-                  ),
-
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text("Continue with Facebook"),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
